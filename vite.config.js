f21dc356ebbfcb32c992daf5505846a22ce1ae28
Prakarsh Singh <prakarshsinghlmp@gmail.com>
2024-06-15 07:35:27 +0530

    url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000/",
+        target: "https://society-sphere-backend.onrender.com/",
         changeOrigin: true,
       },
     },
